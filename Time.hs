a9ac591615c6e8763a9e45f56662f65332b850a2
Chris Done <chrisdone@gmail.com>
2017-03-13 12:16:47 +0000

    Cons differently without deepseq

--- Time.hs
+++ Time.hs
@@ -18,7 +18,7 @@ import qualified Data.Vector.Unboxed as UV
 import           System.Directory
 import           System.Random
 
-data Conser = forall f. NFData (f Int) => Conser String (Int -> f Int)
+data Conser = forall f. NFData (f Int) => Conser String (Int -> IO (f Int)) (Int -> f Int -> f Int)
 data Append = forall f. NFData (f Int) => Append String (Int -> IO (f Int)) (f Int -> f Int -> f Int)
 data Replicator = forall f. NFData (f Int) => Replicator String (Int -> Int -> f Int)
 data Indexing = forall f. NFData (f Int) => Indexing String (IO (f Int)) (f Int -> Int -> Int)
@@ -40,10 +40,10 @@ main = do
     [ bgroup
         "Consing"
         (conses
-           [ Conser "Data.List" conslist
-           , Conser "Data.Vector" consvector
-           , Conser "Data.Vector.Unboxed" consuvector
-           , Conser "Data.Sequence" consseq
+           [ Conser "Data.List" sampleList (:)
+           , Conser "Data.Vector" sampleVector V.cons
+           , Conser "Data.Vector.Unboxed" sampleUVVector UV.cons
+           , Conser "Data.Sequence" sampleSeq (S.<|)
            ])
     , bgroup
         "Replicate"
@@ -132,9 +132,9 @@ main = do
       , Append title payload func <- funcs
       ]
     conses funcs =
-      [ bench (title ++ ":" ++ show i) $ nf func i
+      [ env (sample i) (\p -> bench (title ++ ":" ++ show i) (whnf (\e -> func e p) 1))
       | i <- [10, 100, 1000, 10000]
-      , Conser title func <- funcs
+      , Conser title sample func <- funcs
       ]
     replicators funcs =
       [ bench (title ++ ":" ++ show i) $ nf (\(x, y) -> func x y) (i, 1234)
@@ -211,23 +211,3 @@ sampleUVVector i = evaluate $ force $ UV.fromList (take i (randoms (mkStdGen 0) 
 
 sampleSeq :: Int -> IO (S.Seq Int)
 sampleSeq i = evaluate $ force $ S.fromList (take i (randoms (mkStdGen 0) :: [Int]))
-
-conslist :: Int -> [Int]
-conslist n0 = go n0 []
-  where go 0 acc = acc
-        go n !acc = go (n - 1) (n : acc)
-
-consvector :: Int -> V.Vector Int
-consvector n0 = go n0 V.empty
-  where go 0 acc = acc
-        go n !acc = go (n - 1) (V.cons n acc)
-
-consuvector :: Int -> UV.Vector Int
-consuvector n0 = go n0 UV.empty
-  where go 0 acc = acc
-        go n !acc = go (n - 1) (UV.cons n acc)
-
-consseq :: Int -> S.Seq Int
-consseq n0 = go n0 S.empty
-  where go 0 acc = acc
-        go n !acc = go (n - 1) (n S.<| acc)
