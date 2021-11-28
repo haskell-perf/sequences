ca71c587bb1fc48388b8f7e4aced3ac653fb9e76
Anton-Latukha <anton.latukha@gmail.com>
2021-11-28 16:28:28 +0200

    Time: groups: align

--- Time.hs
+++ Time.hs
@@ -46,152 +46,140 @@ main = do
     [ bgroup
         "Consing"
         (conses
-           [ Conser "Data.List" sampleList (:)
-           , Conser "Data.DList" sampleDList D.cons
-           , Conser "Data.Vector" sampleVector V.cons
-           , Conser "Data.Vector.Unboxed" sampleUVVector UV.cons
+           [ Conser "Data.List"            sampleList     (:)
+           , Conser "Data.DList"           sampleDList    D.cons
+           , Conser "Data.Vector"          sampleVector   V.cons
+           , Conser "Data.Vector.Unboxed"  sampleUVVector UV.cons
            , Conser "Data.Vector.Storable" sampleSVVector SV.cons
-           , Conser "Data.Sequence" sampleSeq (S.<|)
-           , Conser "Data.RRBVector" sampleRRB (RRB.<|)
-           , Conser "Acc" sampleAcc Acc.cons
+           , Conser "Data.Sequence"        sampleSeq      (S.<|)
+           , Conser "Data.RRBVector"       sampleRRB      (RRB.<|)
+           , Conser "Acc"                  sampleAcc      Acc.cons
            ])
     , bgroup
         "Snocing"
         (snocs
-           [ Snocer "Data.DList" sampleDList D.snoc
-           , Snocer "Data.Vector" sampleVector V.snoc
-           , Snocer "Data.Vector.Unboxed" sampleUVVector UV.snoc
+           [ Snocer "Data.DList"           sampleDList    D.snoc
+           , Snocer "Data.Vector"          sampleVector   V.snoc
+           , Snocer "Data.Vector.Unboxed"  sampleUVVector UV.snoc
            , Snocer "Data.Vector.Storable" sampleSVVector SV.snoc
-           , Snocer "Data.Sequence" sampleSeq (S.|>)
-           , Snocer "Data.RRBVector" sampleRRB (RRB.|>)
-           , Snocer "Acc" sampleAcc (flip Acc.snoc)
+           , Snocer "Data.Sequence"        sampleSeq      (S.|>)
+           , Snocer "Data.RRBVector"       sampleRRB      (RRB.|>)
+           , Snocer "Acc"                  sampleAcc      (flip Acc.snoc)
            ])
     , bgroup
         "Indexing"
-        (let size = 10005
-         in indexes
-              [ Indexing "Data.List" (sampleList size) (L.!!)
-              , Indexing "Data.Vector" (sampleVector size) (V.!)
-              , Indexing "Data.Vector.Unboxed" (sampleUVVector size) (UV.!)
-              , Indexing "Data.Vector.Storable" (sampleSVVector size) (SV.!)
-              , Indexing "Data.Sequence" (sampleSeq size) S.index
-              , Indexing "Data.Massiv.Array" (sampleMassivUArray size) M.index'
-              , Indexing "Data.RRBVector" (sampleRRB size) (RRB.!)
-              ])
+        (let size = 10005 in
+         indexes
+           [ Indexing "Data.List"            (sampleList         size) (L.!!)
+           , Indexing "Data.Vector"          (sampleVector       size) (V.!)
+           , Indexing "Data.Vector.Unboxed"  (sampleUVVector     size) (UV.!)
+           , Indexing "Data.Vector.Storable" (sampleSVVector     size) (SV.!)
+           , Indexing "Data.Sequence"        (sampleSeq          size) S.index
+           , Indexing "Data.Massiv.Array"    (sampleMassivUArray size) M.index'
+           , Indexing "Data.RRBVector"       (sampleRRB          size) (RRB.!)
+           ])
     , bgroup
         "Append"
         (appends
-           [ Append "Data.List" sampleList (++) force
-           , Append "Data.DList" sampleDList D.append id
-           , Append "Data.Vector" sampleVector (V.++) id
-           , Append "Data.Vector.Unboxed" sampleUVVector (UV.++) id
-           , Append "Data.Vector.Storable" sampleSVVector (SV.++) id
-           , Append "Data.Sequence" sampleSeq (S.><) id
-           , Append "Data.RRBVector" sampleRRB (RRB.><) id
-           , Append "Acc" sampleAcc (<>) id
+           [ Append "Data.List"            sampleList     (++)      force
+           , Append "Data.DList"           sampleDList    D.append id
+           , Append "Data.Vector"          sampleVector   (V.++)   id
+           , Append "Data.Vector.Unboxed"  sampleUVVector (UV.++)  id
+           , Append "Data.Vector.Storable" sampleSVVector (SV.++)  id
+           , Append "Data.Sequence"        sampleSeq      (S.><)   id
+           , Append "Data.RRBVector"       sampleRRB      (RRB.><) id
+           , Append "Acc"                  sampleAcc      (<>)     id
            ])
     , bgroup
         "Normalization"
         (normalizations
-           [ Normalization "Data.List" sampleList
-           , Normalization "Data.DList" sampleDList
-           , Normalization "Data.Vector" sampleVector
-           , Normalization "Data.Vector.Unboxed" sampleUVVector
+           [ Normalization "Data.List"            sampleList
+           , Normalization "Data.DList"           sampleDList
+           , Normalization "Data.Vector"          sampleVector
+           , Normalization "Data.Vector.Unboxed"  sampleUVVector
            , Normalization "Data.Vector.Storable" sampleSVVector
-           , Normalization "Data.Sequence" sampleSeq
-           , Normalization "Data.Massiv.Array" sampleMassivUArray
-           , Normalization "Data.RRBVector" sampleRRB
-           , Normalization "Data.Acc" sampleAcc
+           , Normalization "Data.Sequence"        sampleSeq
+           , Normalization "Data.Massiv.Array"    sampleMassivUArray
+           , Normalization "Data.RRBVector"       sampleRRB
+           , Normalization "Data.Acc"             sampleAcc
            ])
     , bgroup
         "Length"
         (lengths
-           [ Length "Data.List" sampleList L.length
-           , Length "Data.DList" sampleDList length
-           , Length "Data.Vector" sampleVector V.length
-           , Length "Data.Vector.Unboxed" sampleUVVector UV.length
-           , Length "Data.Vector.Storable" sampleSVVector SV.length
-           , Length "Data.Sequence" sampleSeq S.length
-           , Length "Data.Massiv.Array" sampleMassivUArray M.elemsCount
-           , Length "Data.RRBVector" sampleRRB length
-           , Length "Acc" sampleAcc length
+           [ Length "Data.List"            sampleList         L.length
+           , Length "Data.DList"           sampleDList        length
+           , Length "Data.Vector"          sampleVector       V.length
+           , Length "Data.Vector.Unboxed"  sampleUVVector     UV.length
+           , Length "Data.Vector.Storable" sampleSVVector     SV.length
+           , Length "Data.Sequence"        sampleSeq          S.length
+           , Length "Data.Massiv.Array"    sampleMassivUArray M.elemsCount
+           , Length "Data.RRBVector"       sampleRRB          length
+           , Length "Acc"                  sampleAcc          length
            ])
     , bgroup
         "Stable Sort"
         (sorts
-           [ Sort "Data.List" randomSampleList L.sort
-           , Sort "Data.Vector" randomSampleVector sortVec
-           , Sort "Data.Vector.Unboxed" randomSampleUVVector sortUVec
+           [ Sort "Data.List"            randomSampleList     L.sort
+           , Sort "Data.Vector"          randomSampleVector   sortVec
+           , Sort "Data.Vector.Unboxed"  randomSampleUVVector sortUVec
            , Sort "Data.Vector.Storable" randomSampleSVVector sortSVec
-           , Sort "Data.Sequence" randomSampleSeq S.sort
+           , Sort "Data.Sequence"        randomSampleSeq      S.sort
            ])
     , bgroup
         "Replicate"
         (replicators
-           [ Replicator "Data.List" L.replicate
-           , Replicator "Data.DList" D.replicate
-           , Replicator "Data.Vector" V.replicate
-           , Replicator "Data.Vector.Unboxed" UV.replicate
+           [ Replicator "Data.List"            L.replicate
+           , Replicator "Data.DList"           D.replicate
+           , Replicator "Data.Vector"          V.replicate
+           , Replicator "Data.Vector.Unboxed"  UV.replicate
            , Replicator "Data.Vector.Storable" SV.replicate
-           , Replicator "Data.Sequence" S.replicate
-           , Replicator "Data.RRBVector" RRB.replicate
+           , Replicator "Data.Sequence"        S.replicate
+           , Replicator "Data.RRBVector"       RRB.replicate
            ])
     , bgroup
         "Min"
         (mins
-           [ Min "Data.List" randomSampleList L.minimum
-           , Min "Data.DList" randomSampleDList minimum
-           , Min "Data.Vector" randomSampleVector V.minimum
-           , Min "Data.Vector.Unboxed" randomSampleUVVector UV.minimum
-           , Min "Data.Vector.Storable" randomSampleSVVector SV.minimum
-           , Min "Data.Sequence" randomSampleSeq minimum
-           , Min "Data.Massiv.Array" randomSampleMassivUArray M.minimum'
-           , Min "Data.RRBVector" randomSampleRRB minimum
-           , Min "Acc" randomSampleAcc minimum
+           [ Min "Data.List"            randomSampleList         L.minimum
+           , Min "Data.DList"           randomSampleDList        minimum
+           , Min "Data.Vector"          randomSampleVector       V.minimum
+           , Min "Data.Vector.Unboxed"  randomSampleUVVector     UV.minimum
+           , Min "Data.Vector.Storable" randomSampleSVVector     SV.minimum
+           , Min "Data.Sequence"        randomSampleSeq          minimum
+           , Min "Data.Massiv.Array"    randomSampleMassivUArray M.minimum'
+           , Min "Data.RRBVector"       randomSampleRRB          minimum
+           , Min "Acc"                  randomSampleAcc          minimum
            ])
     , bgroup
         "Max"
         (maxs
-           [ Max "Data.List" randomSampleList L.maximum
-           , Max "Data.DList" randomSampleDList maximum
-           , Max "Data.Vector" randomSampleVector V.maximum
-           , Max "Data.Vector.Unboxed" randomSampleUVVector UV.maximum
-           , Max "Data.Vector.Storable" randomSampleSVVector SV.maximum
-           , Max "Data.Sequence" randomSampleSeq maximum
-           , Max "Data.Massiv.Array" randomSampleMassivUArray M.maximum'
-           , Max "Data.RRBVector" randomSampleRRB maximum
-           , Max "Acc" randomSampleAcc maximum
+           [ Max "Data.List"            randomSampleList         L.maximum
+           , Max "Data.DList"           randomSampleDList        maximum
+           , Max "Data.Vector"          randomSampleVector       V.maximum
+           , Max "Data.Vector.Unboxed"  randomSampleUVVector     UV.maximum
+           , Max "Data.Vector.Storable" randomSampleSVVector     SV.maximum
+           , Max "Data.Sequence"        randomSampleSeq          maximum
+           , Max "Data.Massiv.Array"    randomSampleMassivUArray M.maximum'
+           , Max "Data.RRBVector"       randomSampleRRB          maximum
+           , Max "Acc"                  randomSampleAcc          maximum
            ])
     , bgroup
         "Filter Element"
-        (let size = 10005
-         in removeElems
-              [ RemoveElement "Data.List" (sampleList size) L.filter
-              , RemoveElement "Data.Vector" (sampleVector size) V.filter
-              , RemoveElement
-                  "Data.Vector.Unboxed"
-                  (sampleUVVector size)
-                  UV.filter
-              , RemoveElement
-                  "Data.Vector.Storable"
-                  (sampleSVVector size)
-                  SV.filter
-              , RemoveElement "Data.Sequence" (sampleSeq size) S.filter
-              ])
+        (let size = 10005 in
+         removeElems
+           [ RemoveElement "Data.List"            (sampleList     size) L.filter
+           , RemoveElement "Data.Vector"          (sampleVector   size) V.filter
+           , RemoveElement "Data.Vector.Unboxed"  (sampleUVVector size) UV.filter
+           , RemoveElement "Data.Vector.Storable" (sampleSVVector size) SV.filter
+           , RemoveElement "Data.Sequence"        (sampleSeq      size) S.filter
+           ])
     , bgroup
         "Filter By Index"
-        (let size = 10005
-         in removeByIndexes
-              [ RemoveByIndex "Data.Vector" (sampleVector size) V.ifilter
-              , RemoveByIndex
-                  "Data.Vector.Unboxed"
-                  (sampleUVVector size)
-                  UV.ifilter
-              , RemoveByIndex
-                  "Data.Vector.Storable"
-                  (sampleSVVector size)
-                  SV.ifilter
-              ])
+        (let size = 10005 in
+         removeByIndexes
+           [ RemoveByIndex "Data.Vector"          (sampleVector   size) V.ifilter
+           , RemoveByIndex "Data.Vector.Unboxed"  (sampleUVVector size) UV.ifilter
+           , RemoveByIndex "Data.Vector.Storable" (sampleSVVector size) SV.ifilter
+           ])
     ]
   where
     appends funcs =
